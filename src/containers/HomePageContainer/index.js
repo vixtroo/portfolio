3a38b11f5d50cc7ede99e50752cee5c0fff80d6c
Vincent Castro <110645512+vixtroo@users.noreply.github.com>
2024-01-23 14:21:39 +0800

    implemented email sending

--- src/containers/HomePageContainer/index.js
+++ src/containers/HomePageContainer/index.js
@@ -1,52 +1,62 @@
+import { useState, useEffect } from "react";
+import MainContainer from '@containers/MainContainer'
 import Image from "next/image"
 import Profile from '@assets/Profile 2.jpg'
-import Safe from '@assets/safe.png'
-import Ahg from '@assets/ahg.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowTrendUp } from '@fortawesome/free-solid-svg-icons';
-import { useState } from "react";
+import { faArrowTrendUp, faUser, faEnvelope} from '@fortawesome/free-solid-svg-icons';
+import { experience, scrollToContact, info } from '@utils/indexUtils'
 
 const HomePage = () => {
 
-    const experience = [
-        {company: 'Safe - Midman App', position: 'Frontend Developer | Part-time', date: 'November 2023 - December 2023', img: Safe, alt: 'Safe Logo'},
-        {company: 'AHG Lab', position: 'Frontend Web Developer | Intern', date: 'March 2023 - June 2023', img: Ahg, alt: 'AHG Lab Logo'}
-    ]
+    const [downloaded, setDownloaded] = useState(false);
+  
+const handleDownload = () => {
 
-    const scrollToContact = () => {
-    const contactSection = document.getElementById('contact');
-    
-    if (contactSection) {
-          contactSection.scrollIntoView({ behavior: 'smooth' });
+  if (!downloaded) {
+    setDownloaded(true)
+
+    const link = document.createElement('a')
+    link.href = '/CASTRO, Vincent Patrick - Resume.pdf'
+    link.download = 'CASTRO-VINCENT-PATRICK-RESUME.pdf'
+    document.body.appendChild(link)
+    link.click()
+    document.body.removeChild(link)
+  }
+}
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const sections = document.querySelectorAll('[data-scroll]')
+  
+      sections.forEach((section) => {
+        const rect = section.getBoundingClientRect()
+        const isVisible = rect.top <= window.innerHeight && rect.bottom >= 0
+  
+        if (isVisible) {
+          section.classList.add('fade-in')
+        } else {
+          section.classList.remove('fade-in')
         }
-      };
-    
-      const [downloaded, setDownloaded] = useState(false);
-    
-      const handleDownload = () => {
-    
-        if (!downloaded) {
-          setDownloaded(true)
-    
-          const link = document.createElement('a')
-          link.href = '/RegistrationCertificate_4Y2S.pdf'
-          link.download = 'resume.pdf'
-          document.body.appendChild(link)
-          link.click()
-          document.body.removeChild(link)
-        }
-      };
+      })
+    }
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
     return(
-        <>
-            <section className="flex flex-wrap content-center justify-center w-full h-auto px-24 pb-24 pt-18 gap-x-20" id='about'>
-                <div className="flex flex-wrap content-center justify-between mt-32 w-96">
+        <MainContainer>
+            <section className="flex flex-col-reverse flex-wrap content-center justify-center w-full h-auto px-24 pb-24 md:flex-row pt-18 gap-x-20 fade-in" id='about' data-scroll>
+                <div className="flex flex-wrap content-center justify-between md-20 md:mt-32 w-96">
                     <div className="flex flex-col justify-center mt-6 font-mono text-neutral-200">
                         <h1 className="font-semibold text-8xl">Vincent Castro</h1>
                         <h2 className="text-4xl ">Frontend Developer</h2>
                         <p className="text-xl">2000-present</p>
-                        <div className='flex flex-wrap content-center justify-start w-full gap-2 mt-2'>
-                          <button className='flex flex-wrap content-center justify-center h-10 px-4 duration-100 delay-100 border-2 border-blue-300 hover:bg-blue-300 bg-slate-900 text-neutral-200 hover:border-neutral-200 rounded-xl hover:text-zinc-900' onClick={handleDownload}>{downloaded ? 'Resume Downloaded!' : 'Download Resume'}</button>
+                        <div className='flex flex-wrap content-center justify-center w-full gap-2 mt-4 md:justify-start'>
+                          <button className='flex flex-wrap content-center justify-center h-12 px-4 duration-100 delay-100 border-2 border-blue-300 hover:bg-blue-300 bg-slate-900 text-neutral-200 hover:border-neutral-200 rounded-xl hover:text-zinc-900' onClick={handleDownload}>{downloaded ? 'Resume Downloaded!' : 'Download Resume'}</button>
                           <button className='flex flex-wrap content-center justify-center h-10 px-4 rounded-xl text-neutral-200' onClick={scrollToContact}>Contact me <span className='ml-2'><FontAwesomeIcon icon={faArrowTrendUp}/></span></button>
                         </div>
                     </div>
@@ -55,15 +65,15 @@ const HomePage = () => {
                     <Image src={Profile} width={400} height={400} className="rounded-full grayscale-[100%] hover:grayscale-[0%] transition-all duration-500 ease-in-out drop-shadow-2xl" alt="Profile Photo"/>
                 </div>
             </section>  
-            <section className='flex flex-col items-center w-full h-auto px-24 text-zinc-700' id='experience'>
+            <section className='flex flex-col items-center w-full h-auto px-24 text-zinc-700 fade-in' id='experience' data-scroll>
                 <div className="flex justify-center w-full my-20 font-mono text-4xl font-semibold">
                     Experience
                 </div>
-                <div  className="flex flex-wrap content-center justify-center w-full">
+                <div  className="flex flex-col flex-wrap content-center justify-center w-full md:flex-row gap-y-10">
                     {experience.map((experience, index)=>(
-                        <div className="flex flex-col items-center h-[450px] mx-10 border-2 w-96 border-slate-500 rounded-xl" key={index}>
+                        <div className="flex flex-col items-center h-[450px] mx-10 border-2 w-96 border-slate-500 rounded-xl hover:scale-105 duration-75 exp-card" key={index}>
                             <div className='flex flex-wrap content-center justify-center h-64'>
-                              <Image src={experience.img} width={250} height={250} alt={experience.alt}/>
+                              <Image src={experience.img} width={250} height={250} alt={experience.alt} className='duration-200 exp-image'/>
                             </div>
                             <h3 className='font-mono text-xl font-semibold'>{experience.company}</h3>
                             <p className='mt-2 font-mono'>{experience.position}</p>
@@ -71,10 +81,47 @@ const HomePage = () => {
                         </div>
                     ))}
                 </div>
-            </section>  
-            <section className='w-full h-[500px]' id='projects'></section>  
-            <section className='w-full h-[500px]' id='contact'></section> 
-        </>
+            </section>   
+            <section className='flex flex-col items-center justify-between w-full text-zinc-700 h-[500px] fade-in' id='projects' data-scroll>
+                <div className='flex justify-center w-full my-20 font-mono text-4xl font-semibold'>
+                  Projects
+                </div>
+            </section>
+            <section className='flex flex-col items-center justify-between w-full font-mono text-zinc-700 fade-in' id='contact' data-scroll>
+                <div className='flex justify-center w-full my-20 text-4xl font-semibold'>
+                  Contact
+                </div>
+                <div className="flex flex-col flex-wrap content-center justify-center w-full mb-20 md:flex-row gap-y-10">
+                        <form action=''>
+                          <div className="flex flex-col items-center p-4 mx-10 border-2 border-slate-500 rounded-xl">
+                              <h1 className='text-2xl font-bold tracking-wide'>Email me</h1>
+                              <div className='flex flex-col content-center justify-center p-4 gap-y-4'>
+                                <label htmlFor="name" style={{ position: 'relative' }}>
+                                  <input type="text" id="name" placeholder="Name" style={{ paddingLeft: '40px' }}/>
+                                  <FontAwesomeIcon icon={faUser} style={{ position: 'absolute', left: '15px', top: '50%', transform: 'translateY(-50%)', color:'gray'}}/>
+                                </label>
+                                <label htmlFor="email" style={{ position: 'relative' }}>
+                                  <input type="text" id="email" placeholder="Email" style={{ paddingLeft: '40px' }}/>
+                                  <FontAwesomeIcon icon={faEnvelope} style={{ position: 'absolute', left: '15px', top: '50%', transform: 'translateY(-50%)', color:'gray'}}/>
+                                </label>
+                                <textarea placeholder="Message" id="message" name="message" rows="4" cols="30" className='w-[280px] pl-3 py-[15px] rounded-xl'></textarea>
+                                <button className='flex flex-wrap content-center justify-center duration-100 delay-100 border-2 border-blue-300 hover:bg-blue-300 bg-slate-900 text-neutral-200 hover:border-slate-800 rounded-xl hover:text-zinc-900' type='submit'>Send Message</button>
+                              </div>
+                          </div>
+                        </form>
+                        <div className="flex flex-col items-center mx-10 w-96">
+                            <h1 className='px-10 font-mono text-lg font-semibold border-b-2'>Get in touch</h1>
+                            <div className='flex flex-wrap content-start justify-start w-full px-12'>
+                              <ul className='mt-4 leading-8'>
+                                {info.map((info, index)=>(
+                                  <li key={index} className='flex'><FontAwesomeIcon icon={info.icon} className='h-10 p-4 mt-2 border rounded-full'/><p className='mt-3.5 ml-4'>{info.info}</p></li>
+                                ))}
+                              </ul>
+                            </div>
+                        </div>
+                </div>
+            </section> 
+      </MainContainer>
     )
 }
 
---
+++ src/utils/ContactForm/index.js
@@ -0,0 +1,119 @@
+import React, { useState } from "react";
+import emailjs from "emailjs-com";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser, faEnvelope } from "@fortawesome/free-solid-svg-icons";
+
+const ContactForm = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const templateParams = {
+        name: formData.name,
+        email: formData.email,
+        message: formData.message,
+      };
+
+      await emailjs.send(
+        "service_e9g4wcq",
+        "template_40bclqq",
+        templateParams,
+        "K-d0fTm-8nMbhsvvZ"
+      );
+
+      console.log("Email sent successfully");
+      alert("Email sent successfully");
+      setFormData({
+        name: "",
+        email: "",
+        message: "",
+      });
+    } catch (error) {
+      console.error("Error sending email", error);
+      alert("Error sending email");
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="flex flex-col items-center p-4 mx-10 border-2 border-slate-500 rounded-xl">
+        <h1 className="text-2xl font-bold tracking-wide">Email me</h1>
+        <div className="flex flex-col content-center justify-center p-4 gap-y-4">
+          <label htmlFor="name" style={{ position: "relative" }}>
+            <input
+              type="text"
+              id="name"
+              placeholder="Name (Optional)"
+              style={{ paddingLeft: "40px" }}
+              onChange={handleChange}
+              value={formData.name}
+            />
+            <FontAwesomeIcon
+              icon={faUser}
+              style={{
+                position: "absolute",
+                left: "15px",
+                top: "50%",
+                transform: "translateY(-50%)",
+                color: "gray",
+              }}
+            />
+          </label>
+          <label htmlFor="email" style={{ position: "relative" }}>
+            <input
+              type="text"
+              id="email"
+              placeholder="Email"
+              style={{ paddingLeft: "40px" }}
+              onChange={handleChange}
+              value={formData.email}
+              required
+            />
+            <FontAwesomeIcon
+              icon={faEnvelope}
+              style={{
+                position: "absolute",
+                left: "15px",
+                top: "50%",
+                transform: "translateY(-50%)",
+                color: "gray",
+              }}
+            />
+          </label>
+          <textarea
+            placeholder="Message"
+            id="message"
+            name="message"
+            rows="4"
+            cols="30"
+            className="w-[280px] pl-3 py-[15px] rounded-xl"
+            onChange={handleChange}
+            value={formData.message}
+            required
+          ></textarea>
+          <button
+            className="flex flex-wrap content-center justify-center duration-100 delay-100 border-2 border-blue-300 hover:bg-blue-300 bg-slate-900 text-neutral-200 hover:border-slate-800 rounded-xl hover:text-zinc-900"
+            type="submit"
+          >
+            Send Message
+          </button>
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default ContactForm;
