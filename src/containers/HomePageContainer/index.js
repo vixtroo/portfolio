8b8ee67a02f3227a42b95b6a05a47e5cadf03c08
Vincent Castro <110645512+vixtroo@users.noreply.github.com>
2024-03-03 18:36:31 +0800

    UI fixes

--- src/containers/HomePageContainer/index.js
+++ src/containers/HomePageContainer/index.js
@@ -1,14 +1,10 @@
-import { useState, useEffect } from "react";
-import MainContainer from "@containers/MainContainer";
 import Image from "next/image";
 import Profile from "@assets/Profile 2.jpg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faArrowTrendUp,
-  faUser,
-  faEnvelope,
-} from "@fortawesome/free-solid-svg-icons";
+import { faArrowTrendUp } from "@fortawesome/free-solid-svg-icons";
+import { useState, useEffect } from "react";
 import { experience, scrollToContact, info } from "@utils/indexUtils";
+import ContactForm from "@utils/ContactForm";
 
 const HomePage = () => {
   const [downloaded, setDownloaded] = useState(false);
@@ -50,7 +46,7 @@ const HomePage = () => {
   }, []);
 
   return (
-    <MainContainer>
+    <>
       <section
         className="flex flex-col-reverse flex-wrap content-center justify-center w-full h-auto px-24 pb-24 md:flex-row pt-18 gap-x-20 fade-in"
         id="about"
@@ -63,7 +59,7 @@ const HomePage = () => {
             <p className="text-xl">2000-present</p>
             <div className="flex flex-wrap content-center justify-center w-full gap-2 mt-4 md:justify-start">
               <button
-                className="flex flex-wrap content-center justify-center h-12 px-4 duration-100 delay-100 border-2 border-blue-300 hover:bg-blue-300 bg-slate-900 text-neutral-200 hover:border-neutral-200 rounded-xl hover:text-zinc-900"
+                className="flex flex-wrap content-center justify-center h-12 px-4 duration-100 border-2 border-blue-300 hover:bg-blue-300 bg-slate-900 text-neutral-200 hover:border-neutral-200 rounded-xl hover:text-zinc-900"
                 onClick={handleDownload}
               >
                 {downloaded ? "Resume Downloaded!" : "Download Resume"}
@@ -140,63 +136,7 @@ const HomePage = () => {
           Contact
         </div>
         <div className="flex flex-col flex-wrap content-center justify-center w-full mb-20 md:flex-row gap-y-10">
-          <form action="">
-            <div className="flex flex-col items-center p-4 mx-10 border-2 border-slate-500 rounded-xl">
-              <h1 className="text-2xl font-bold tracking-wide">Email me</h1>
-              <div className="flex flex-col content-center justify-center p-4 gap-y-4">
-                <label htmlFor="name" style={{ position: "relative" }}>
-                  <input
-                    type="text"
-                    id="name"
-                    placeholder="Name"
-                    style={{ paddingLeft: "40px" }}
-                  />
-                  <FontAwesomeIcon
-                    icon={faUser}
-                    style={{
-                      position: "absolute",
-                      left: "15px",
-                      top: "50%",
-                      transform: "translateY(-50%)",
-                      color: "gray",
-                    }}
-                  />
-                </label>
-                <label htmlFor="email" style={{ position: "relative" }}>
-                  <input
-                    type="text"
-                    id="email"
-                    placeholder="Email"
-                    style={{ paddingLeft: "40px" }}
-                  />
-                  <FontAwesomeIcon
-                    icon={faEnvelope}
-                    style={{
-                      position: "absolute",
-                      left: "15px",
-                      top: "50%",
-                      transform: "translateY(-50%)",
-                      color: "gray",
-                    }}
-                  />
-                </label>
-                <textarea
-                  placeholder="Message"
-                  id="message"
-                  name="message"
-                  rows="4"
-                  cols="30"
-                  className="w-[280px] pl-3 py-[15px] rounded-xl"
-                ></textarea>
-                <button
-                  className="flex flex-wrap content-center justify-center duration-100 delay-100 border-2 border-blue-300 hover:bg-blue-300 bg-slate-900 text-neutral-200 hover:border-slate-800 rounded-xl hover:text-zinc-900"
-                  type="submit"
-                >
-                  Send Message
-                </button>
-              </div>
-            </div>
-          </form>
+          <ContactForm />
           <div className="flex flex-col items-center mx-10 w-96">
             <h1 className="px-10 font-mono text-lg font-semibold border-b-2">
               Get in touch
@@ -217,7 +157,7 @@ const HomePage = () => {
           </div>
         </div>
       </section>
-    </MainContainer>
+    </>
   );
 };
 
